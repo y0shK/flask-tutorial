ae7ff0665e67a32d5ea7d88278fa29407062553d
Yash Karandikar <ynkarandikar@gmail.com>
2023-04-23 11:45:06 -0700

    follow tutorial optional parts

---
+++ hello_app/views.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import render_template
+from datetime import datetime
+from . import app
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about/")
+def about():    
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    
+    return render_template(
+        "hello_there.html",
+        name = name,
+        date = datetime.now()
+    )
+
+# send a static JSON file
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
